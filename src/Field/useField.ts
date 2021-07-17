2684c03975ba00673ebd9c9127c00fc879941141
Tony <jim.casey321@gmail.com>
2021-07-17 17:27:46 +0300

    add random function and use it on every init and move decoupled logic to utils

--- src/Field/useField.ts
+++ src/Field/useField.ts
@@ -1,6 +1,6 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 
-import { createField } from './utils';
+import { createField, willRemainAlive } from './utils';
 import { Cell } from '../types';
 
 interface Input {
@@ -15,49 +15,12 @@ interface Field {
     cells: Cell[][];
 }
 
-const INTERVAL_TIME = 3000 as const;
+const INTERVAL_TIME = 400 as const;
 
 const useField = ({ width, height }: Input): Output => {
     const [field, setField] = useState<Field>({
         cells: createField(width, height),
     });
-
-    const countNeighbours = (x: number, y: number, cells: Cell[][]): number => {
-        const theoreticNeighbourCoords = [
-            [x - 1, y - 1],
-            [x - 1, y],
-            [x - 1, y + 1],
-            [x, y - 1],
-            [x, y + 1],
-            [x + 1, y - 1],
-            [x + 1, y],
-            [x + 1, y + 1],
-        ];
-
-        const aliveNeighboursCount = theoreticNeighbourCoords
-            .map(([x, y]) => {
-                if (!cells[y]) {
-                    return undefined;
-                }
-
-                return cells[y][x]?.isAlive;
-            })
-            .filter((item) => !!item).length;
-
-        return aliveNeighboursCount;
-    };
-
-    const checkIfLivesOn = (aliveNeighbours: number, isCellAlive: boolean): boolean => {
-        return isCellAlive ? [2, 3].includes(aliveNeighbours) : aliveNeighbours === 3;
-    };
-
-    const willRemainAlive = (x: number, y: number, cells: Cell[][]): boolean => {
-        const aliveNeighboursCount = countNeighbours(x, y, cells);
-
-        const isCellAlive = cells[y][x].isAlive;
-
-        return checkIfLivesOn(aliveNeighboursCount, isCellAlive);
-    };
 
     const calculateNextIteration = (cells: Cell[][]) => {
         const newCells = cells.map((row) => {
--- src/Field/utils.ts
+++ src/Field/utils.ts
@@ -1,9 +1,15 @@
+import { Cell } from '../types';
+
+export const getRandomBool = (): boolean => {
+    return Math.random() < 0.5;
+};
+
 export const createField = (width: number, height: number) => {
     const cells: undefined[][] = new Array(height).fill(new Array(width).fill(undefined));
 
     const result = cells.map((row, y) => {
         return row.map((_, x) => ({
-            isAlive: false,
+            isAlive: getRandomBool(),
             x,
             y,
         }));
@@ -11,20 +17,86 @@ export const createField = (width: number, height: number) => {
 
     // TODO: Remove testing code
 
-    result[5][4] = {
-        ...result[5][4],
-        isAlive: true,
-    };
+    // test 1
 
-    result[5][5] = {
-        ...result[5][5],
-        isAlive: true,
-    };
+    // result[5][4] = {
+    //     ...result[5][4],
+    //     isAlive: true,
+    // };
 
-    result[5][6] = {
-        ...result[5][6],
-        isAlive: true,
-    };
+    // result[5][5] = {
+    //     ...result[5][5],
+    //     isAlive: true,
+    // };
+
+    // result[5][6] = {
+    //     ...result[5][6],
+    //     isAlive: true,
+    // };
+
+    // test 2
+
+    // result[1][4] = {
+    //     ...result[1][4],
+    //     isAlive: true,
+    // };
+
+    // result[2][5] = {
+    //     ...result[2][5],
+    //     isAlive: true,
+    // };
+
+    // result[3][5] = {
+    //     ...result[3][5],
+    //     isAlive: true,
+    // };
+
+    // result[3][4] = {
+    //     ...result[3][4],
+    //     isAlive: true,
+    // };
+
+    // result[3][3] = {
+    //     ...result[3][3],
+    //     isAlive: true,
+    // };
 
     return result;
+};
+
+export const countNeighbours = (x: number, y: number, cells: Cell[][]): number => {
+    const theoreticNeighbourCoords = [
+        [x - 1, y - 1],
+        [x - 1, y],
+        [x - 1, y + 1],
+        [x, y - 1],
+        [x, y + 1],
+        [x + 1, y - 1],
+        [x + 1, y],
+        [x + 1, y + 1],
+    ];
+
+    const aliveNeighboursCount = theoreticNeighbourCoords
+        .map(([x, y]) => {
+            if (!cells[y]) {
+                return undefined;
+            }
+
+            return cells[y][x]?.isAlive;
+        })
+        .filter((item) => !!item).length;
+
+    return aliveNeighboursCount;
+};
+
+export const checkIfLivesOn = (aliveNeighbours: number, isCellAlive: boolean): boolean => {
+    return isCellAlive ? [2, 3].includes(aliveNeighbours) : aliveNeighbours === 3;
+};
+
+export const willRemainAlive = (x: number, y: number, cells: Cell[][]): boolean => {
+    const aliveNeighboursCount = countNeighbours(x, y, cells);
+
+    const isCellAlive = cells[y][x].isAlive;
+
+    return checkIfLivesOn(aliveNeighboursCount, isCellAlive);
 };
