0f5cb739194c6dcde6565a96114d155d64d3d347
Tony <jim.casey321@gmail.com>
2021-07-17 18:24:19 +0300

    add tests for countNeighbours checkIfLivesOn and getKey utils

--- src/Field/__test__/utils.test.ts
+++ src/Field/__test__/utils.test.ts
@@ -1,33 +1,74 @@
-import { create } from 'domain';
-import { getRandomBool, createField } from '../utils';
+import { getRandomBool, createField, countNeighbours, checkIfLivesOn, getKey } from '../utils';
+
+const mockField = [
+    [
+        { y: 0, x: 0, isAlive: true },
+        { y: 0, x: 1, isAlive: true },
+        { y: 0, x: 2, isAlive: true },
+    ],
+    [
+        { y: 1, x: 0, isAlive: true },
+        { y: 1, x: 1, isAlive: true },
+        { y: 1, x: 2, isAlive: true },
+    ],
+    [
+        { y: 2, x: 0, isAlive: true },
+        { y: 2, x: 1, isAlive: true },
+        { y: 2, x: 2, isAlive: true },
+    ],
+];
 
 describe('utils', () => {
     describe('createField', () => {
         it('should return double dimension array with correct structure of cells', () => {
             const temp = Math.random;
-            Math.random = jest.fn().mockReturnValue(0.6);
+            Math.random = jest.fn().mockReturnValue(0.4);
 
-            const expectedResult = [
-                [
-                    { y: 0, x: 0, isAlive: false },
-                    { y: 0, x: 1, isAlive: false },
-                    { y: 0, x: 2, isAlive: false },
-                ],
-                [
-                    { y: 1, x: 0, isAlive: false },
-                    { y: 1, x: 1, isAlive: false },
-                    { y: 1, x: 2, isAlive: false },
-                ],
-                [
-                    { y: 2, x: 0, isAlive: false },
-                    { y: 2, x: 1, isAlive: false },
-                    { y: 2, x: 2, isAlive: false },
-                ],
-            ];
-
-            expect(createField(3, 3)).toEqual(expectedResult);
+            expect(createField(3, 3)).toEqual(mockField);
 
             Math.random = temp;
+        });
+    });
+
+    describe('countNeighbours', () => {
+        it('should return 3 for cell in the corner if all cells are alive', () => {
+            const expected = countNeighbours(0, 0, mockField);
+
+            expect(expected).toEqual(3);
+        });
+
+        it('should return 8 for cell in the corner', () => {
+            const expected = countNeighbours(1, 1, mockField);
+
+            expect(expected).toEqual(8);
+        });
+    });
+
+    describe('checkIfLivesOn', () => {
+        it('should become alive if it was alive and has 2 neighbours', () => {
+            expect(checkIfLivesOn(2, true)).toBe(true);
+        });
+
+        it('should become alive if it was dead and has 3 neighbours', () => {
+            expect(checkIfLivesOn(3, false)).toBe(true);
+        });
+
+        it('should die if it was alive and has 1 neighbour', () => {
+            expect(checkIfLivesOn(1, true)).toBe(false);
+        });
+
+        it('should die if it was alive and has 4 neigbours', () => {
+            expect(checkIfLivesOn(1, true)).toBe(false);
+        });
+    });
+
+    describe('getKey', () => {
+        it('should return 1 2', () => {
+            expect(getKey({ x: 2, y: 1 })).toBe('1 2');
+        });
+
+        it('should return undefined undefined', () => {
+            expect(getKey(undefined)).toBe('undefined undefined');
         });
     });
 
